57f2513dc399ef99cbd472d9777e7e0786e0f61c
Swyamk <swyamkumar2428@gmail.com>
2025-02-17 23:16:45 +0530

    addition of the testcase

--- testcase/for_regular.go
+++ testcase/for_regular.go
@@ -1,3 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
 
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("UP   :", i)
+	}
+	x := 42
+	for ; x > 0; x -= 10 {
+		fmt.Println("DOWN :", x)
+	}
+}
