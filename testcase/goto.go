92ccb03ee93506e7ffeea266f16a721445e248bb
Swyamk <swyamkumar2428@gmail.com>
2025-02-19 02:26:03 +0530

    updation of the testcase

--- testcase/goto.go
+++ testcase/goto.go
@@ -3,3 +3,15 @@ package main
 import (
 	"fmt"
 )
+
+func main() {
+	goto first
+second:
+	fmt.Println("SECOND.")
+	goto done
+first:
+	fmt.Println("FIRST.")
+	goto second
+done:
+	fmt.Println("DONE.")
+}
