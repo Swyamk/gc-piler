822bd41cdd1d414885c92d595a78b49f2fcbf9e9
Swyamk <swyamkumar2428@gmail.com>
2025-02-21 22:49:31 +0530

    adding info

--- testcase/if.go
+++ testcase/if.go
@@ -1,3 +1,20 @@
 package main
 
 import "fmt"
+
+func main() {
+	x := 4
+	fmt.Print("The number + 3 is... ")
+	if x+3 > 7 {
+		fmt.Print("larger than seven and ")
+	} else {
+		fmt.Print("smaller or equal to seven and ")
+	}
+	if x+3 > 7 {
+		fmt.Println("larger than seven.")
+	} else if x < 7 {
+		fmt.Println("smaller than seven.")
+	} else {
+		fmt.Println("smaller or equal to seven.")
+	}
+}
