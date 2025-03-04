ec1ee424b5f1fa67ca80f77324e6f7a47b9f2857
Swyamk <swyamkumar2428@gmail.com>
2025-03-04 13:42:55 +0530

    adding info

--- testcase/types.go
+++ testcase/types.go
@@ -11,3 +11,14 @@ type (
 	f32 float32
 	i16 int16
 )
+
+type snakestring string
+
+func main() {
+	var sss snakestring = "sssssss"
+	var (
+		n u8 = 255
+	)
+	var i int = int(n)
+	fmt.Println(sss, i)
+}
